d68a92d8f4651ba58a9d218bf98a22255848e5ec
mariacabriles29 <mecp2992@gmail.com>
2023-11-20 22:36:41 -0400

    feat: :art: new login with google

--- next.config.js
+++ next.config.js
@@ -1,12 +1,16 @@
 module.exports = {
-    env: {
-        "BASE_URL": "http://localhost:3000",
-        "MONGODB_URL": "YOUR_MONGODB_URL",
-        "ACCESS_TOKEN_SECRET":"tDBRwuITk1QmosuvG6ggqvILvSceul8DoI9FsGIVXH9XgUPSti",
-        "REFRESH_TOKEN_SECRET":"o5veWsgoGPrHRNqpPoWXDmA0Aaa5e8gWlyfN",
-        "PAYPAL_CLIENT_ID": "AeNAjY2TANlE4SSDbq6yYRedpw5Tscbc7z-Xxx0MBTCTcUwtTCu9zX7w9i7PiKOu5NaJZnOMEXOiDc35",
-        "CLOUD_UPDATE_PRESET": "next_store",
-        "CLOUD_NAME": "ddtwmoh7j",
-        "CLOUD_API": "https://api.cloudinary.com/v1_1/ddtwmoh7j/image/upload"
-    }
-}
+  env: {
+    BASE_URL: "http://localhost:3000",
+    MONGODB_URL: "YOUR_MONGODB_URL",
+    ACCESS_TOKEN_SECRET: "tDBRwuITk1QmosuvG6ggqvILvSceul8DoI9FsGIVXH9XgUPSti",
+    REFRESH_TOKEN_SECRET: "o5veWsgoGPrHRNqpPoWXDmA0Aaa5e8gWlyfN",
+    PAYPAL_CLIENT_ID:
+      "AeNAjY2TANlE4SSDbq6yYRedpw5Tscbc7z-Xxx0MBTCTcUwtTCu9zX7w9i7PiKOu5NaJZnOMEXOiDc35",
+    CLOUD_UPDATE_PRESET: "next_store",
+    CLOUD_NAME: "ddtwmoh7j",
+    CLOUD_API: "https://api.cloudinary.com/v1_1/ddtwmoh7j/image/upload",
+    GOOGLE_CLIENT_ID:
+      "755232149573-c53r7qlcvavrq66vku7mss642aqbepkv.apps.googleusercontent.com",
+    GOOGLE_CLIENT_SECRET: "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",
+  },
+};
--- pages/signin.js
+++ pages/signin.js
@@ -5,6 +5,10 @@ import { DataContext } from "../store/GlobalState";
 import { postData } from "../utils/fetchData";
 import Cookie from "js-cookie";
 import { useRouter } from "next/router";
+import { GoogleLogin } from "@react-oauth/google";
+import { GoogleOAuthProvider } from "@react-oauth/google";
+import jwt from "jsonwebtoken";
+import valid from "../utils/valid";
 
 const Signin = () => {
   const initialState = { email: "", password: "" };
@@ -20,6 +24,68 @@ const Signin = () => {
     const { name, value } = e.target;
     setUserData({ ...userData, [name]: value });
     dispatch({ type: "NOTIFY", payload: {} });
+  };
+
+  const userRegister = async (newCurrentUser) => {
+    const errMsg = valid(
+      newCurrentUser.given_name,
+      newCurrentUser.family_name,
+      " ",
+      " ",
+      " ",
+      newCurrentUser.email,
+      "123456",
+      "123456"
+    );
+
+    if (errMsg) return dispatch({ type: "NOTIFY", payload: { error: errMsg } });
+
+    dispatch({ type: "NOTIFY", payload: { loading: true } });
+
+    const currentUser = {
+      name: newCurrentUser.given_name,
+      last_name: newCurrentUser.family_name,
+      phone_number: " ",
+      address: " ",
+      country_of_residence: " ",
+      email: newCurrentUser.email,
+      password: "123456",
+      cf_password: "123456",
+    };
+
+    const res = await postData("auth/register", currentUser);
+
+    if (res.success) {
+      dispatch({ type: "NOTIFY", payload: { success: res.msg } });
+      signInGoogle({ email: newCurrentUser.email, password: "123456" });
+    } else {
+      setUserData({ email: newCurrentUser.email, password: "123456" });
+      signInGoogle({ email: newCurrentUser.email, password: "123456" });
+    }
+  };
+
+  const signInGoogle = async (newUser) => {
+    dispatch({ type: "NOTIFY", payload: { loading: true } });
+    const res = await postData("auth/login", newUser);
+
+    if (res.err)
+      return dispatch({ type: "NOTIFY", payload: { error: res.err } });
+    dispatch({ type: "NOTIFY", payload: { success: res.msg } });
+
+    dispatch({
+      type: "AUTH",
+      payload: {
+        token: res.access_token,
+        user: res.user,
+      },
+    });
+
+    Cookie.set("refreshtoken", res.refresh_token, {
+      path: "api/auth/accessToken",
+      expires: 7,
+    });
+
+    localStorage.setItem("firstLogin", true);
   };
 
   const handleSubmit = async (e) => {
@@ -50,57 +116,78 @@ const Signin = () => {
   useEffect(() => {
     if (Object.keys(auth).length !== 0) router.push("/");
   }, [auth]);
-
+  //304531247476-58f940f3b0dgrupg95cdo8b51fspupdv.apps.googleusercontent.com
   return (
-    <div>
-      <Head>
-        <title>Sign in Page</title>
-      </Head>
+    <GoogleOAuthProvider clientId="304531247476-58f940f3b0dgrupg95cdo8b51fspupdv.apps.googleusercontent.com">
+      <div>
+        <Head>
+          <title>Sign in Page</title>
+        </Head>
 
-      <form
-        className="mx-auto my-4"
-        style={{ maxWidth: "500px" }}
-        onSubmit={handleSubmit}
-      >
-        <div className="form-group">
-          <label htmlFor="exampleInputEmail1">Email address</label>
-          <input
-            type="email"
-            className="form-control"
-            id="exampleInputEmail1"
-            aria-describedby="emailHelp"
-            name="email"
-            value={email}
-            onChange={handleChangeInput}
+        <div />
+
+        <form
+          className="mx-auto my-4"
+          style={{ maxWidth: "500px" }}
+          onSubmit={handleSubmit}
+        >
+          <div className="form-group">
+            <label htmlFor="exampleInputEmail1">Email address</label>
+            <input
+              type="email"
+              className="form-control"
+              id="exampleInputEmail1"
+              aria-describedby="emailHelp"
+              name="email"
+              value={email}
+              onChange={handleChangeInput}
+            />
+            <small id="emailHelp" className="form-text text-muted">
+              We'll never share your email with anyone else.
+            </small>
+          </div>
+          <div className="form-group">
+            <label htmlFor="exampleInputPassword1">Password</label>
+            <input
+              type="password"
+              className="form-control"
+              id="exampleInputPassword1"
+              name="password"
+              value={password}
+              onChange={handleChangeInput}
+            />
+          </div>
+
+          <button
+            type="submit"
+            className="btn btn-dark w-100"
+            onClick={() => signIn}
+          >
+            Login
+          </button>
+
+          <GoogleLogin
+            onSuccess={(credentialResponse) => {
+              console.log(credentialResponse);
+
+              const newCurrentUser = jwt.decode(credentialResponse.credential);
+              userRegister(newCurrentUser);
+            }}
+            onError={() => {
+              console.log("Login Failed");
+            }}
+            useOneTap
           />
-          <small id="emailHelp" className="form-text text-muted">
-            We'll never share your email with anyone else.
-          </small>
-        </div>
-        <div className="form-group">
-          <label htmlFor="exampleInputPassword1">Password</label>
-          <input
-            type="password"
-            className="form-control"
-            id="exampleInputPassword1"
-            name="password"
-            value={password}
-            onChange={handleChangeInput}
-          />
-        </div>
 
-        <button type="submit" className="btn btn-dark w-100">
-          Login
-        </button>
-
-        <p className="my-2">
-          You don't have an account?{" "}
-          <Link href="/register">
-            <button style={{ color: "crimson" }}>Register Now</button>
-          </Link>
-        </p>
-      </form>
-    </div>
+          <p className="my-2">
+            You don't have an account?{" "}
+            <Link href="/register">
+              <button style={{ color: "crimson" }}>Register Now</button>
+            </Link>
+          </p>
+        </form>
+      </div>
+    </GoogleOAuthProvider>
   );
 };
 
